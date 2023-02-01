f4491f85caaed46ee3918aecb445cdfb5fc31e17
YVD7 <yashdawande7@gmail.com>
2023-02-01 13:27:03 +0530

    update

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
-with open("README.md","r",encoding="utf-8") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 __version__ = "0.0.0"
 
-REPO_NAME = "FSDS_NOV_deepClassifier"
-AUTHOR_USER_NAME = "YVD7"
+REPO_NAME = "FSDS_NOV_deepCNNClassifier"
+AUTHOR_USER_NAME = "c17hawke"
 SRC_REPO = "deepClassifier"
-AUTHOR_EMAIL = "yashdawande7@gmail.com"
+AUTHOR_EMAIL = "sunny.c17hawke@gmail.com"
 
 setuptools.setup(
     name=SRC_REPO,
@@ -17,11 +17,11 @@ setuptools.setup(
     author_email=AUTHOR_EMAIL,
     description="A small python package for CNN app",
     long_description=long_description,
-    long_description_content = "text/markdown",
+    long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
-        "Bug Tracker":f"https://github.com{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir={"":"src"},
+    package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
 )
--- src/deepClassifier/pipeline/stage_01_data_ingestion copy.py
+++ src/deepClassifier/pipeline/stage_01_data_ingestion copy.py
@@ -3,7 +3,6 @@ from deepClassifier.components import DataIngestion
 from deepClassifier import logger
 
 STAGE_NAME = "Data Ingestion stage"
-
 def main():
     config = ConfigurationManager()
     data_ingestion_config = config.get_data_ingestion_config()
@@ -11,11 +10,12 @@ def main():
     data_ingestion.download_file()
     data_ingestion.unzip_and_clean()
 
+
 if __name__ == '__main__':
     try:
-        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
         main()
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx===========x")
     except Exception as e:
         logger.exception(e)
         raise e
